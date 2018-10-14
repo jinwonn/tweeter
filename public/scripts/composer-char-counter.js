7a110ab9526ea3414648e1b6d2951658fe09e8ad
Daniel <d.t.nguyen@live.com>
2018-10-14 23:10:43 +0000

    added tweet formatting

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,6 +1,13 @@
 $(document).ready(function() {
-  console.log("dom loaded")
-  $(.new-tweet).on('keyup', '#tweet-composer textarea', function() {
-    console.log("key pressed");
-  })
-};)
+  var maxLength = 140;
+  $('#tweet-input').keyup(function() {
+    var chartyped = $(this).val().length;
+    var totchartyped = maxLength - chartyped
+    if (totchartyped < 0) {
+      $("#counter").css("color", "#ff0000");
+    } else {
+      $("#counter").css("color", "");
+    }
+    $('#counter').text(totchartyped);
+  });
+});

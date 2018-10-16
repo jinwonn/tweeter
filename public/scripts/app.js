45d7f3227f2ce71d02e1ae5990e79b10cd1d2ec8
Daniel <d.t.nguyen@live.com>
2018-10-16 04:40:08 +0000

    “cleaned”

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -20,37 +20,37 @@ const tweetData = {
 }
 
 
-  function createTweetElement () {
-    var $header = $("<header>");
-    var $avatar = $("<img>").attr('src', "/images/icon.png");
-    var $username = $("<h1>").text("Bill Feilds");
-    var $handle = $("<h2>").text("@MrFields");
+  function createTweetElement (twtobj) {
+
+    //creation of tweet header
+    let $header = $("<header>");
+    let $avatar = $("<img>").attr('src', twtobj.user.avatars.regular);
+    let $username = $("<h1>").text(twtobj.user.name);
+    let $handle = $("<h2>").text(twtobj.user.handle);
     $header.append($avatar, $username, $handle);
-return $header
+    
+    //creation of tweet body
+    let $tweetbody = $("<div>").addClass("tweet-body");
+    let $tweet = $("<p>").text(twtobj.content.text);
+    $tweetbody.append($tweet);
+
+    //creation of footer
+    let $footer = $("<footer>");
+    let $timestamp = $("<p>").text("10 days ago");
+    let $buttons = $("<span>")
+    let $flag = $('<i>').addClass('fa fa-flag').attr('aria-hidden', 'true');
+    let $retweet = $('<i>').addClass('fa fa-retweet').attr('aria-hidden', 'true');
+    let $heart = $('<i>').addClass('fa fa-heart').attr('aria-hidden', 'true');
+    $buttons.append($flag, $retweet, $heart);
+    $footer.append($timestamp, $buttons);
+    
+    //creation of final output 
+    let $output = $("<section>").addClass("tweet");
+    $output.append($header, $tweetbody, $footer);
+    return $output;
   }
-// createTweetElement(tweetData);
 
-// Test / driver code (temporary)
-// console.log($tweet); // to see what it looks like
-$('#tweet-container').append(createTweetElement());
-
-// <!-- <header>
-// <img src="/images/icon.png" />
-// <h1>Bill Fields</h1>
-// <h2>@MrFields</h2>
-// </header>
-// <div class="tweet-body">
-// <p>
-// Little tweet here Little tweet here Little tweet here Little tweet here Little tweet here Little tweet here Little tweet here Little tweet here Little tweet here Little tweet here Little tweet here Little tweet here 
-// </p>
-// </div>
-// <footer>
-// <p>
-// 10 days ago
-// </p>
-// <span>
-//   <i class="fa fa-flag" aria-hidden="true"></i>
-//   <i class="fa fa-retweet" aria-hidden="true"></i>
-//   <i class="fa fa-heart" aria-hidden="true"></i>
-// </span>
-// </footer> -->
+  //appending created tweet after DOM is loaded
+$(document).ready(function() {
+$('.container').append(createTweetElement(tweetData));
+});
